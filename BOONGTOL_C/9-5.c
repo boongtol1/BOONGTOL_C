990ba775a0cfc5a8ac2c3fc15c7215249919e0bf
boongtol1 <boongtol@icloud.com>
2024-06-05 07:58:41 +0900

    boongtol

--- BOONGTOL_C/9-5.c
+++ BOONGTOL_C/9-5.c
@@ -10,7 +10,7 @@ int main(void)
 	int* pi = &in;
 	double* pd = &db;
 
-	printf("char형 변수의 주소 크기 : %d\n", sizeof(&ch));
+	printf("char형 변수의 주소 크기 : %d\n", sizeof(&ch));	// 숫자 크기로 그 사이즈를 측정!
 	printf("int형 변수의 주소 크기 : %d\n", sizeof(&in));
 	printf("double형 변수의 주소 크기 : %d\n", sizeof(&db));
 
@@ -24,3 +24,6 @@ int main(void)
 
 	return 0;
 }
+// 따라서, 포인터가 차지하는 바이트 수는 다음과 같습니다:
+// 32비트 시스템: 포인터는 4바이트를 차지합니다.
+// 64비트 시스템: 포인터는 8바이트를 차지합니다.
